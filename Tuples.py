cc91955ae7b4ab5468606c6821b54e57ef9ec3a6
Gurdeep Singh <gharika@gmail.com>
2018-11-15 21:48:40 +0530

    Base

--- Tuples.py
+++ Tuples.py
@@ -1,4 +1,13 @@
 t = "a", "b", "c"
 print(t)
 
-#Tuples can't be assigned once they are created
+# Tuples can't be assigned once they are created
+t = t[0], 2, t[2]
+print(t)
+
+# assign multiple variables
+a = b = c = d = 12
+a, b = 12, 14
+
+value_a, value_b, value_c = t
+print(value_b)
