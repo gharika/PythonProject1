8a0b8fa66ed7745d0e88c4d300dd0d95ce5e4ea1
Gurdeep Singh <gharika@gmail.com>
2018-11-16 13:23:23 +0530

    changes

---
+++ binary.py
@@ -0,0 +1,10 @@
+# dictionary
+fruits = {"orange": "description for orange",
+          "apple": "description for apple"}
+
+print(fruits["apple"])
+if "apple" in fruits:
+    del fruits["apple"]
+fruits.clear()
+
+
